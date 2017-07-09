a640bf78c416d75b4fa4e50230468d3bdb4210a7
BonDyka <bondarew2507@gmail.com>
2017-07-09 10:33:16 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,19 @@
 package ru.job4j;
-
+/**
+ * Class Calculate. The solution task from chapter_001 lesson 1(JVM. Byte code).
+ * 
+ * @author aondarev
+ * @since 08.07.2017
+ */
 public class Calculate {
-	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+	/**
+	 * The method takes a string parameter and return new string where value of the parameter
+	 * is represent three times through spaces.
+	 *
+	 * @param value a string for changing.
+	 * @return String represent value three times throug spaces.
+	 */
+	public String echo(String value) {
+		return String.format("%s %s %s", value,  value, value);
 	}
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Alexander Bondarev (mailto:bondarew2507@gmail.com)
+ * @version 2.0
+ * @since 09.072017
+ */
+package ru.job4j;
