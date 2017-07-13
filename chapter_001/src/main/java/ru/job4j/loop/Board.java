21a7605ac9a65e1beddfa13436de68508fde6f7e
BonDyka <bondarew2507@gmail.com>
2017-07-13 18:48:52 +0300

    4.3. Построить шахматную доску в псевдографике. [#13559]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,33 @@
+package ru.job4j.loop;
+
+/**
+ * The class represent cell-board.
+ *
+ * @author abondarev
+ * @since 13.07.2017.
+ */
+public class Board {
+
+	/**
+	 * The method paints cell board in pseudo-graphics.
+	 *
+	 * @param width is width of the board.
+	 * @param height is height of the board.
+	 * @return a string that represent the board.
+	 */
+	public String paint(int width, int height) {
+		StringBuilder result = new StringBuilder();
+		String newLine = System.getProperty("line.separator");
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				if (i % 2 != 0 && j % 2 != 0 || i % 2 == 0 && j % 2 == 0) {
+					result.append("X");
+				} else {
+					result.append(" ");
+				}
+			}
+			result.append(newLine);
+		}
+		return result.toString();
+	}
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * The class tests behaviuor of the board class.
+ *
+ * @author abondarev.
+ * @since 13.07.2017.
+ */
+public class BoardTest {
+
+	/**
+	 * The test board with size 3*3.
+	 */
+	@Test
+	public void whenPaintBoardWithWidthThreeAndHeightThreeThenStringWithThreeColsAndThreeRows() {
+        Board board = new Board();
+        String result = board.paint(3, 3);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("X X%s X %sX X%s", line, line, line);
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * The test board with size 5*4.
+     */
+    @Test
+    public void whenPaintBoardWithWidthFiveAndHeightFourThenStringWithFiveColsAndFourRows() {
+        Board board = new Board();
+        String result = board.paint(5, 4);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("X X X%s X X %sX X X%s X X %s",
+        								line, line, line, line, line);
+        assertThat(result, is(expected));
+    }
+}
