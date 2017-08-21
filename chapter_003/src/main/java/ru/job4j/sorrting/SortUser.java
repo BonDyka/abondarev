9fc93b1e1c04ff30b5163b87973d0861dcde1428
BonDyka <bondarew2507@gmail.com>
2017-08-21 15:21:59 +0300

    Sort list using comparator. Task #10036. Into class SortUser added methods sortByNameLength() and sortByAllFields().

--- chapter_003/src/main/java/ru/job4j/sorrting/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sorrting/SortUser.java
@@ -1,8 +1,6 @@
 package ru.job4j.sorrting;
 
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * Shows sort of collection.
@@ -25,5 +23,42 @@ public class SortUser {
             result.add(user);
         }
         return result;
+    }
+
+    /**
+     * Sorts list of users by name length of user.
+     *
+     * @param list for sorting.
+     * @return sorted list.
+     */
+    public List<User> sortByNameLength(List<User> list) {
+        Comparator<User> comparator = new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getName().length() - o2.getName().length();
+            }
+        };
+        Collections.sort(list, comparator);
+        return list;
+    }
+
+    /**
+     * Sorts list of users by all fields.
+     *
+     * @param list for sorting.
+     * @return sorted list.
+     */
+    public List<User> sortByAllFields(List<User> list) {
+        Comparator<User> comparator = new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                int firstLength = o1.getName().length();
+                int secondLength = o2.getName().length();
+                return (firstLength - secondLength == 0) ? o1.getAge() - o2.getAge()
+                                                         : firstLength - secondLength;
+            }
+        };
+        Collections.sort(list, comparator);
+        return list;
     }
 }
--- chapter_003/src/test/java/ru/job4j/sorrting/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sorrting/SortUserTest.java
@@ -2,10 +2,7 @@ package ru.job4j.sorrting;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -29,6 +26,42 @@ public class SortUserTest {
         Set<User> expect = new TreeSet<>();
         expect.add(list.get(1));
         expect.add(list.get(0));
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Tests method sortByNameLength().
+     */
+    @Test
+    public void sortByNameLength() {
+        List<User> list = Arrays.asList(new User("Tom", 24), new User("Barry", 10),
+                                        new User("Jim", 22), new User("John", 31)
+        );
+        SortUser filter = new SortUser();
+        List<User> expect = new ArrayList<>();
+        expect.add(list.get(0));
+        expect.add(list.get(2));
+        expect.add(list.get(3));
+        expect.add(list.get(1));
+        List<User> result = filter.sortByNameLength(list);
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Tests method sort().
+     */
+    @Test
+    public void sortByAllFields() {
+        List<User> list = Arrays.asList(new User("Tom", 24), new User("John", 10),
+                new User("Jim", 22), new User("Tony", 31)
+        );
+        SortUser filter = new SortUser();
+        List<User> expect = new ArrayList<>();
+        expect.add(list.get(2));
+        expect.add(list.get(0));
+        expect.add(list.get(1));
+        expect.add(list.get(3));
+        List<User> result = filter.sortByAllFields(list);
         assertThat(result, is(expect));
     }
 }
