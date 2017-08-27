5123099b51b00fb61b079bdb3f572ddc05628294
BonDyka <bondarew2507@gmail.com>
2017-08-27 10:56:44 +0300

    Added classes for task #151
    Signed-off-by: BonDyka <bondarew2507@gmail.com>

---
+++ chapter_005pro/src/main/java/ru/job4j/iterators/PrimeIterator.java
@@ -0,0 +1,78 @@
+package ru.job4j.iterators;
+
+import java.util.Iterator;
+
+import static java.lang.Math.sqrt;
+
+/**
+ * The class implements iterator for prime numbers.
+ *
+ * @author abondarev.
+ * @since 27.08.2017.
+ */
+public class PrimeIterator implements Iterator {
+
+    /**
+     * Contains values for iteration.
+     */
+    private int[] values;
+
+    /**
+     * Next position pointer.
+     */
+    private int pointer;
+
+    /**
+     * The constructor. Takes as parameter an array for iteration.
+     *
+     * @param values an array for iteration.
+     */
+    public PrimeIterator(int[] values) {
+        this.values = values;
+    }
+
+    /**
+     * Returns <tt>true</tt> if next element is prime number.
+     *
+     * @return <tt>true</tt> if next element is prime number.
+     */
+    @Override
+    public boolean hasNext() {
+        return isPrime(values[pointer]);
+    }
+
+    /**
+     * Returns element from array in wrapper or null if element not found.
+     *
+     * @return element from array in wrapper or null if element not found.
+     */
+    @Override
+    public Integer next() {
+        Integer result = null;
+        for (; pointer < values.length; pointer++) {
+            if (isPrime(values[pointer])) {
+                result = values[pointer];
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Checks are number prime or not.
+     *
+     * @param number it's number for checking.
+     * @return <tt>true</tt> if number is prime.
+     */
+    private boolean isPrime(int number) {
+        int counter = 0; // Count amount of found dividers.
+        if (number > 2 || number % 2 != 0) {
+            for (int i = 2; i <= sqrt(number) + 1; i++) {
+                if (number % i == 0) {
+                    counter++;
+                }
+            }
+        }
+        return counter < 1;
+    }
+}
---
+++ chapter_005pro/src/test/java/ru/job4j/iterators/PrimeIteratorTest.java
@@ -0,0 +1,64 @@
+package ru.job4j.iterators;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created for testing PrimeIterator class.
+ *
+ * @author abondarev.
+ * @since 27.08.2017.
+ */
+public class PrimeIteratorTest {
+
+    /**
+     * Tests method hasNext() when next number is prime.
+     */
+    @Test
+    public void whenCalledHasNextIfNextIsPrimeNumberShouldReturnTrue() {
+        Iterator it = new PrimeIterator(new int[] {2});
+
+        boolean result = it.hasNext();
+
+        assertThat(result, is(true));
+    }
+
+    /**
+     * Tests method hasNext() when next number is not prime.
+     */
+    @Test
+    public void whenCalledHasNextIfNextIsNotPrimeNumberShouldReturnFalse() {
+        Iterator it = new PrimeIterator(new int[] {6});
+
+        boolean result = it.hasNext();
+
+        assertThat(result, is(false));
+    }
+
+    /**
+     * Tests method next() when there is prime number.
+     */
+    @Test
+    public void whenCalledNextIfPrimeThereIsShouldReturnFirstPrimeElement() {
+        Iterator it = new PrimeIterator(new int[] {4, 7, 5});
+
+        int result = (Integer) it.next();
+
+        assertThat(result, is(7));
+    }
+
+    /**
+     * Tests method next() when there isn't prime number.
+     */
+    @Test
+    public void whenCalledNextIfPrimeThereIsNotShouldReturnNull() {
+        Iterator it = new PrimeIterator(new int[] {4});;
+
+        assertNull(it.next());
+    }
+}
