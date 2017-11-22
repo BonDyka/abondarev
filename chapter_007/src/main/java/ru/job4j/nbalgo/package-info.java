ab2746dfda1ced9e47bf4c15f68983b57d857d1a
BonDyka <bondarew2507@gmail.com>
2017-11-22 16:32:50 +0300

    package-info added

---
+++ chapter_007/src/main/java/ru/job4j/nbalgo/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package created for implementation {@link ru.job4j.nbalgo.ConcurrentCash}
+ * based on {@link java.util.concurrent.ConcurrentHashMap}
+ *
+ * @author Alexander Bondarev(mailto:bondarew2507@gmail.com).
+ * @since 22.11.2017.
+ */
+package ru.job4j.nbalgo;
