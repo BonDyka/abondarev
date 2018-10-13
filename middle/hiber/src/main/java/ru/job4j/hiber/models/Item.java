bd1f896564e762d4ace922996f5d5c4252735cea
BonDyka <bondarew2507@gmail.com>
2018-10-13 11:56:02 +0300

    Create base structure for hibernate project

---
+++ middle/hiber/src/main/java/ru/job4j/hiber/models/Item.java
@@ -0,0 +1,42 @@
+package ru.job4j.hiber.models;
+
+import java.sql.Timestamp;
+
+public class Item {
+    private long id;
+    private String description;
+    private Timestamp created;
+    private boolean done;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
---
+++ middle/hiber/src/main/java/ru/job4j/hiber/persistence/GenericDao.java
@@ -0,0 +1,4 @@
+package ru.job4j.hiber.persistence;
+
+public class GenericDao {
+}
