36e4fca207c6757599b8a1faaea025043d591697
BonDyka <bondarew2507@gmail.com>
2018-11-05 13:35:28 +0300

    Change repain of page on reload in main.js

--- middle/hiber/src/main/webapp/js/main.js
+++ middle/hiber/src/main/webapp/js/main.js
@@ -2,24 +2,8 @@ $(document).ready(function () {
     listItems($('.show').prop('checked'), showItemsList);
     changeShowed();
     saveChanged();
-
     createItem();
 });
-
-function createItem() {
-    $('[type=button]').on('click', function () {
-        var formData = $('form').serializeArray();
-        $.ajax({
-            url: '/create',
-            method:'post',
-            data : formData,
-            complete : function () {
-                clearTable();
-                listItems($('.show').prop('checked'), showItemsList);
-            }
-        })
-    })
-}
 
 function listItems(showAll, callback) {
     $('#msg').remove();
@@ -37,6 +21,45 @@ function listItems(showAll, callback) {
     })
 }
 
+function saveChanged() {
+    $(document).on('change', '.done', function () {
+        var itemData = $(this).closest('.row').find('td').first().text();
+        $.ajax({
+            url: '/edit',
+            method: 'post',
+            data: {'id' : itemData, 'done' : $(this).prop('checked')},
+            complete: function () {
+                document.location.reload(true);
+            }
+        });
+    })
+}
+
+function changeShowed() {
+    $('#show').on('change', function () {
+        clearTable();
+        listItems($(this).prop('checked'), showItemsList);
+    })
+
+}
+
+function createItem() {
+    $('[type=button]').on('click', function () {
+        var formData = $('form').serializeArray();
+        if (formData[0].value === '') {
+            console.log('field empty');
+        }
+        $.ajax({
+            url: '/create',
+            method:'post',
+            data : formData,
+            complete : function () {
+                document.location.reload(true);
+            }
+        });
+    })
+}
+
 function showItemsList(list) {
     list.forEach(function (item) {
         var row = "<tr class='row'><td>" + item.id +"</td><td>" + item.description
@@ -51,29 +74,6 @@ function showItemsList(list) {
     });
 }
 
-function changeShowed() {
-    $('#show').on('change', function () {
-        clearTable();
-        listItems($(this).prop('checked'), showItemsList);
-    })
-
-}
-
 function clearTable() {
     $('.row').remove();
-}
-
-function saveChanged() {
-    $(document).on('change', '.done', function () {
-        var itemData = $(this).closest('.row').find('td').first().text();
-        $.ajax({
-            url: '/edit',
-            method: 'post',
-            data: {'id' : itemData, 'done' : $(this).prop('checked')},
-            complete: function () {
-                clearTable();
-                listItems($('.show').prop('checked'), showItemsList);
-            }
-        });
-    })
 }
