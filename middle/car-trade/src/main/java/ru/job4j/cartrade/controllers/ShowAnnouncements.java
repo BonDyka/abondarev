139b93adbb1b5cead265b1586f2a0cf15cf454f0
BonDyka <bondarew2507@gmail.com>
2019-04-20 12:05:46 +0300

    Create module for web part of the task. Tsk#4747

---
+++ middle/car-trade/src/main/java/ru/job4j/cartrade/controllers/ShowAnnouncements.java
@@ -0,0 +1,26 @@
+package ru.job4j.cartrade.controllers;
+
+import com.google.gson.Gson;
+import ru.job4j.carstore.models.annotated.Announcement;
+import ru.job4j.carstore.persistence.GenericDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class ShowAnnouncements extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        List<Announcement> list = new GenericDao<>(Announcement.class).readAll();
+        Gson gson = new Gson();
+
+        PrintWriter pw = new PrintWriter(resp.getOutputStream());
+        pw.append(gson.toJson(list));
+        pw.flush();
+        pw.close();
+    }
+}
---
+++ middle/car-trade/src/main/java/ru/job4j/cartrade/controllers/SignIn.java
@@ -0,0 +1,43 @@
+package ru.job4j.cartrade.controllers;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import ru.job4j.carstore.models.annotated.User;
+import ru.job4j.carstore.persistence.GenericDao;
+import ru.job4j.carstore.persistence.criterias.UserByLogin;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SignIn extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        User loginUser = new User();
+        loginUser.setLogin(login);
+
+        // The method readByCriteria with criteria UserByLogin returns list with single User entity.
+        User result = new GenericDao<>(User.class).readByCriteria(new UserByLogin(loginUser)).get(0);
+
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        if (result != null && password.equals(result.getPassword())) {
+                writer.append(new Gson().toJson(result));
+                resp.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            JsonObject msgJson = new JsonObject();
+            msgJson.addProperty("msg", "Incorrect login or password!");
+            writer.append(msgJson.toString());
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+
+        writer.flush();
+        writer.close();
+    }
+
+}
